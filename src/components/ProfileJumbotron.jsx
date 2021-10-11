7cd77fc61ef40bc44ea7495982b2deb4f22f8bcb
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-11 10:11:23 +0100

    Updated

--- src/components/ProfileJumbotron.jsx
+++ src/components/ProfileJumbotron.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
+
 class ProfileJumbotron extends React.Component {
   render() {
     return (
@@ -11,14 +12,20 @@ class ProfileJumbotron extends React.Component {
                         <Card.Body>
                             <img className="profile-banner" src="./assets/profilebanner.png" />
                             <img className="profile-picture" src="./assets/profilepicture.png" />
-                <Card.Title>Card Title</Card.Title>
+                            <div class="profile-info">
+<Card.Title>Diego Banovaz</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">
-                  Card Subtitle
+                  COO & Co-Founder @ Strive School (YC S20) - WE ARE HIRING
+                </Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">
+                  Berlin, Berlin, Germany <a href="#">Contact Info</a>
                 </Card.Subtitle>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </Card.Text>
+                            </div>
+                
                 <Card.Link href="#">Card Link</Card.Link>
                 <Card.Link href="#">Another Link</Card.Link>
               </Card.Body>
