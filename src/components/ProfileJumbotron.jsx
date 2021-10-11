70d6697641e65b5482d69b08df1e6be1fe4826aa
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-11 11:22:26 +0100

    Updated - jumbotron completed

--- src/components/ProfileJumbotron.jsx
+++ src/components/ProfileJumbotron.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 class ProfileJumbotron extends React.Component {
   render() {
@@ -17,36 +17,67 @@ class ProfileJumbotron extends React.Component {
                   className="profile-picture"
                   src="./assets/profilepicture.jfif"
                 />
-                <div className="profile-info">
-                  <Card.Title>Diego Banovaz</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    COO & Co-Founder @ Strive School (YC S20) - WE ARE HIRING
-                  </Card.Subtitle>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    Berlin, Berlin, Germany <a href="#">Contact Info</a>
-                  </Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                </div>
-                <div className="mutual-connections">
-                  <Card.Subtitle className="mb-2 text-muted">
-                    500+ connections
-                  </Card.Subtitle>
-                  <img
-                    className="mutual-connections-images"
-                    src="./assets/Tobias.jfif"
-                  ></img>
-                  <img
-                    className="mutual-connections-images-2"
-                    src="./assets/stefano.jfif"
-                                ></img>
-                                <a className="mutual-connections-link" href="#">12 mutual connections: Tobia De Angelis, Stefano Casasasola and 10 others</a>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-xs-8">
+                      <div className="profile-info">
+                        <Card.Title>Diego Banovaz</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">
+                          COO & Co-Founder @ Strive School (YC S20) - WE ARE
+                          HIRING
+                        </Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">
+                          Berlin, Berlin, Germany <a href="#">Contact Info</a>
+                        </Card.Subtitle>
+                      </div>
+                    </div>
+                    <div class="col-xs-4">
+                      <div className="profile-experiences-preview">
+                        <div>
+                          <img
+                            className="experiences-img"
+                            src="./assets/striveschool.jfif"
+                          ></img>{" "}
+                          Strive School
+                        </div>
+                        <div>
+                          <img
+                            className="experiences-img"
+                            src="./assets/ycombinator.jfif"
+                          ></img>{" "}
+                          Y Combinator
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
 
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <div className="mutual-connections">
+                  <Card.Subtitle className="mb-2 connections-title">
+                    500+ connections
+                  </Card.Subtitle>
+                  <div className="d-flex">
+                    <img
+                      className="mutual-connections-images"
+                      src="./assets/Tobias.jfif"
+                    ></img>
+                    <img
+                      className="mutual-connections-images-2"
+                      src="./assets/stefano.jfif"
+                    ></img>
+                    <a className="mutual-connections-link text-muted" href="#">
+                      <p className="mutual">
+                        12 mutual connections: Tobia De Angelis, Stefano
+                        Casasasola and 10 others
+                      </p>
+                    </a>
+                  </div>
+                </div>
+                <div className="profile-buttons-section">
+                  <Button variant="primary buttons">Connect</Button>
+                  <Button variant="outline-dark buttons">Message</Button>
+                  <Button variant="outline-dark buttons">More</Button>
+                </div>
               </Card.Body>
             </Card>
           </Col>
