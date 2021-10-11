3a00aa5bb57ee167352536596ebe2dccd860331f
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-11 16:00:23 +0100

    Updated

--- src/components/ProfileJumbotron.jsx
+++ src/components/ProfileJumbotron.jsx
@@ -13,7 +13,7 @@ class ProfileJumbotron extends React.Component {
   async componentDidMount() {
     try {
       const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/profile/me",
+        "https://striveschool-api.herokuapp.com/api/profile/{5d84937322b7b54d848eb41b}",
         {
           headers: new Headers({
             Authorization:
