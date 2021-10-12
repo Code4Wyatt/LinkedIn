b3d8822f576b68075886bee418f49a57bc504078
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-12 14:07:41 +0100

    Update

--- src/components/ProfileJumbotron.jsx
+++ src/components/ProfileJumbotron.jsx
@@ -1,16 +1,63 @@
+<<<<<<< Updated upstream
 import React,{useState} from 'react'; 
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+=======
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Dropdown,
+  DropdownButton,
+  ListGroup,
+} from "react-bootstrap";
+>>>>>>> Stashed changes
 
 class ProfileJumbotron extends React.Component {
    constructor() {
     super();
+<<<<<<< Updated upstream
        this.state = {
            data: [],
            error: false,
        };
+=======
+    this.state = {
+      data: [],
+      error: false,
+      toggle: false,
+    };
+>>>>>>> Stashed changes
   }
 
+  async fetchProfiles(props) {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/: + ${this.props.userId}`,
+        {
+          headers: new Headers({
+            Authorization:
+              "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MWYwMWE4OTBjYzAwMTVjZjA3ZWYiLCJpYXQiOjE2MzM5NTE0ODksImV4cCI6MTYzNTE2MTA4OX0.vx77x7lAtcX0LJjTGsp1uSzKGgE5K7MlKFsN70cMX5Q",
+          }),
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        this.setState({ error: false, data });
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+      this.setState({ error: true });
+    }
+  }
+
+  toggleSelected = (e) => e.preventDefault;
+
   async componentDidMount() {
+    // this.fetchProfiles();
     try {
       const response = await fetch(
         "https://striveschool-api.herokuapp.com/api/profile/me",
@@ -34,8 +81,13 @@ class ProfileJumbotron extends React.Component {
 
   render() {
     return (
+<<<<<<< Updated upstream
       <Container>
         <Row className="row-cols-8">
+=======
+      <Container className="profile-container">
+        <Row>
+>>>>>>> Stashed changes
           <Col>
             <Card style={{ width: "50rem" }}>
               <Card.Body>
@@ -59,7 +111,14 @@ class ProfileJumbotron extends React.Component {
                           {this.state.data.bio}
                         </Card.Subtitle>
                         <Card.Subtitle className="mb-2 text-muted">
+<<<<<<< Updated upstream
                           {this.state.data.area} <a href="#">Contact Info</a>
+=======
+                          {this.state.data.area}{" "}
+                          <a className="contact-info-link" href="#">
+                            Contact Info
+                          </a>
+>>>>>>> Stashed changes
                         </Card.Subtitle>
                       </div>
                     </div>
@@ -105,10 +164,158 @@ class ProfileJumbotron extends React.Component {
                     </a>
                   </div>
                 </div>
-                <div className="profile-buttons-section">
+                <div className="profile-buttons-section d-flex">
                   <Button variant="primary buttons">Connect</Button>
                   <Button variant="outline-dark buttons">Message</Button>
-                  <Button variant="outline-dark buttons">More</Button>
+                  <DropdownButton
+                    align="end"
+                    id="dropdown-menu-align-end"
+                    title="Add Selection"
+                  >
+                    <Dropdown.ItemText>Add Selection</Dropdown.ItemText>
+                    <Dropdown.Item className="dropdown" as="button">
+                      <div className="justify-content-space-between btns">
+                        <p>Intro</p>{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-chevron-down"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                          />
+                        </svg>
+                      </div>
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button">
+                      About{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                    <ListGroup>
+  <ListGroup.Item>Cras justo odio</ListGroup.Item>
+  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+  <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+  <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+</ListGroup>
+                    <Dropdown.Item as="button">
+                      Featured{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      as="button"
+                      /*onclick={(e) => {
+                        this.toggleSelected;
+                      }} */
+                    >
+                      Background{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button">
+                      Skills{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button">
+                      Accomplishments{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button">
+                      Additional information{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button">
+                      Supported languages{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </Dropdown.Item>
+                  </DropdownButton>
                 </div>
               </Card.Body>
             </Card>
