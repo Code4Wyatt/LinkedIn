8581b28316b9a1aa2fb8432b6c90b9115d56e2d3
clodsmr <87863094+clodsmr@users.noreply.github.com>
2021-10-15 13:53:38 +0200

    same edit and add button on profile

--- src/components/ProfileJumbotron.jsx
+++ src/components/ProfileJumbotron.jsx
@@ -141,7 +141,7 @@ function ProfileJumbotron(props) {
             onClick={() => setShowModal(true)}
           />
         </Row>
-        <Row style={{ padding: "15px", paddingBottom: "0", paddingTop: "0" }}>
+        <Row className="d-flex justify-content-between"style={{ padding: "15px", paddingBottom: "0", paddingTop: "0" }}>
           <div>
             <p>
               {data.name} {data.surname}
@@ -155,16 +155,16 @@ function ProfileJumbotron(props) {
               </a>
             </p>
           </div>
-          <Col>
+          <Button id="addExButton" onClick={handleShow}>
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
-                fill="currentColor"
+                fill="Grey"
                 class="bi bi-pencil"
                 viewBox="0 0 16 16"
-                onClick={handleShow}
+                
               >
                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
               </svg>
@@ -262,7 +262,7 @@ function ProfileJumbotron(props) {
                 </Modal.Body>
               </Modal>
             </div>
-          </Col>
+          </Button>
         </Row>
         <Row style={{ padding: "15px", paddingBottom: "0" }}>
           <div className="col-xs-4">
